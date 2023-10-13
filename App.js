49b160ecea1a0e3706f02abe77925dbf308aece5
payal pokale <78795031+payalpokale-7@users.noreply.github.com>
2023-10-13 15:21:59 +0530

    Foldername

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Progressbar from "./Progressbar";
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+		<Progressbar />
+    </div>
+  );
+}
+
+export default App;
